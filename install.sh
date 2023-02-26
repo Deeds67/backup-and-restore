92a482435b1378b57a4b2eec6f662773c4d14d5f
pierre <pierremarais67@gmail.com>
2023-02-26 10:54:36 +0100

    Install and configure pyenv

--- install.sh
+++ install.sh
@@ -1,6 +1,28 @@
 ## Update
 sudo apt -y update && sudo apt -y upgrade
 
+## Install basics
+sudo apt-get install -y htop vim tmux jq ncdu pigz sudo pv unzip curl wget git
+
+## Install packages needed for pyenv
+sudo apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+## Install pyenv
+## https://github.com/pyenv/pyenv#basic-github-checkout
+curl https://pyenv.run | bash
+
+## Add pyenv to path if it does not exist
+LINE='export PYENV_ROOT="$HOME/.pyenv"'
+
+CONTENTS='export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"'
+
+FILE=~/.zshrc
+
+# Check if line exists, if it does not, add contents to file
+grep -qF -- "$LINE" "$FILE" || echo "$CONTENTS" >> "$FILE"
+
 ## Install ZSH
 sudo apt -y install zsh
 
