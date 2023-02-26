d0e5231d279ee2a4bbfbf12fa13ca68c97e898dd
pierre <pierremarais67@gmail.com>
2023-02-26 11:06:28 +0100

    Added backup and restore for gnome terminal settings

--- backup.sh
+++ backup.sh
@@ -4,3 +4,6 @@
 ## Backup hotkeys, etc:
 dconf dump / > saved_settings.dconf
 
+## Backup gnome terminal settings
+dconf dump /org/gnome/terminal/ > gnome_terminal_settings_backup.txt
+
--- restore.sh
+++ restore.sh
@@ -4,3 +4,6 @@ dconf load / < saved_settings.dconf
 ## Restore guake config
 #guake --restore-preferences guake_prefs
 
+## Restore gnome terminal settings
+dconf load /org/gnome/terminal/ < gnome_terminal_settings_backup.txt
+
